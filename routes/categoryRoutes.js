4994e9b67780f08961e1ebc6df6d446ae1a46209
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-02-05 22:55:14 +0100

    update cotegorycontrolles : daletecategory

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,23 +2,27 @@ const express = require("express");
 const router = express.Router();
 const {getAllCategories,getCategoryById,getDeletedCategories,createCategory,updateCategory,deleteCategory,restoreCategory,addTag,getTags,getTagsByCategory,getTagById,updateTag,getDeletedTags,restoreTag,deleteTag} = require("../controllers/categoryContollers");
 
-// category
+// post 
 router.post("/add", createCategory)
-router.get("/", getAllCategories)
-router.get("/deleted", getDeletedCategories)
-router.get("/:id", getCategoryById)
-router.patch("/:id",  updateCategory)
-router.delete("/:id", deleteCategory)
-router.patch("/restore/:id", restoreCategory)
-
-// Tags
 router.post("/add-tag", addTag)
+
+// Get
+router.get("/", getAllCategories)
 router.get("/tags", getTags)
+router.get("/deleted", getDeletedCategories)
+router.get("/tags/deleted", getDeletedTags)
 router.get("/:id/tags", getTagsByCategory)
 router.get("/tags/:id", getTagById)
-router.get("/tags/deleted", getDeletedTags)
+router.get("/:id", getCategoryById)
+
+// Patch
+router.patch("/:id",  updateCategory)
 router.patch("/tags/:id", updateTag)
+router.patch("/restore/:id", restoreCategory)
 router.patch("/tags/restore/:id", restoreTag)
+
+// Delete
+router.delete("/:id", deleteCategory)
 router.delete("/tags/:id", deleteTag)
 
 
