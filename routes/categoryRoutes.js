3e3afbd6a28139cb9f2cbf4b3cbee5cd7d458365
Bouih32 <enamto.bouih@gmail.com>
2025-02-05 15:59:32 +0000

    feat: add GET endpoint for retrieving categories

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const prisma = require("../utils/client");
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const data = await prisma.category.findMany();
+    res.status(200).send({ data, message: "ghaya bla di3aya" });
+  } catch (error) {
+    res.status(500).send(error, { message: "oops " });
+  }
+});
 module.exports = router;
