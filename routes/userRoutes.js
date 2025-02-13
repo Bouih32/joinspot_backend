9a8b5668ff2bc9373db0d10db5a02d5388b13f38
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-02-13 17:14:59 +0100

    feat: add validation and Handlerrors and updated routes to use it

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {isAuthenticated} = require("../middlewares/Authentication")
+const { isAuthenticated } = require("../middlewares/Authentication");
 const router = express.Router();
 const {
   loginUser,
@@ -7,18 +7,22 @@ const {
   getUserData,
   logOut,
   updateUserData,
-  changePassword
+  changePassword,
 } = require("../controllers/userControllers");
+const {
+  loginValidation,
+  registerValidation,
+  validationandHandlerrors,
+} = require("../utils/validation");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/logout", logOut);
+router.post("/register",registerValidation,validationandHandlerrors, registerUser);
+router.post("/login",loginValidation,validationandHandlerrors, loginUser);
+router.post("/logout",validationandHandlerrors, logOut);
 
-router.get("/profil",isAuthenticated, getUserData);
+router.get("/profil", isAuthenticated,validationandHandlerrors, getUserData);
 
-router.put("/change-password",isAuthenticated, changePassword);
+router.put("/change-password", isAuthenticated,validationandHandlerrors, changePassword);
 
-router.patch("/edit-profil",isAuthenticated, updateUserData);
-
+router.patch("/edit-profil", isAuthenticated,validationandHandlerrors, updateUserData);
 
 module.exports = router;
