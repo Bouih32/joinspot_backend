ee27b249be18bf03f6dac63a57a9c7ef015b75b6
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-03-02 15:27:30 +0000

    feat: implement comprehensive post management routes et controllers

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,1 +1,153 @@
-const prisma = require("../utils/primsaClient");
+const prisma = require("../utils/client");
+
+const createPost = async (req, res) => {
+  try {
+    const { bannerPic, description, categoryId } = req.body;
+    const newPost = await prisma.post.create({
+      data: {
+        bannerPic,
+        description,
+        category: { connect: { categoryId: categoryId } },
+        user: { connect: { userId: req.user.userId } },
+      },
+    });
+    return res
+      .status(201)
+      .json({ message: "Post created successfully", post: newPost });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Failed to create post", error: error.message });
+  }
+};
+const addTagToPost = async (req, res) => {
+    try {
+        const { tagIds } = req.body;
+        console.log(tagIds, "tagids");
+        if (!Array.isArray(tagIds) || tagIds.length === 0) {
+          return res.status(400).json({
+            message: "Tags must be an array and not empty",
+            received: req.body,
+          });
+        }
+    
+        const existingPostTags = await prisma.postTags.findMany({
+          where: {
+            postId: req.params.postId,
+            tagId: { in: tagIds },
+          },
+        });
+    
+        if (existingPostTags.length > 0) {
+          return res.status(400).json({
+            message: "Tags already exist",
+          });
+        }
+        const newPostTags = tagIds.filter(
+          (id) => !existingPostTags.some((tag) => tag.tagId === id)
+        );
+        const postTags = await prisma.postTags.createMany({
+          data: newPostTags.map((tagId) => ({
+            postId: req.params.postId,
+            tagId,
+          })),
+        });
+        if (postTags.length === 0) {
+          return res.status(404).json({ message: "Tags not found" });
+        }
+    
+        return res
+          .status(200)
+          .json({ message: "Tags added to activity", postTags });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+          message: "Failed to add tags to activity",
+          error: error.message,
+        });
+      }
+  };
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        category: {
+          select: {
+            categoryName: true,
+          },
+        },
+        user: {
+          select: {
+            userName: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+            comment: true,
+            share: true,
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+    if (posts.length === 0) {
+      return res.status(404).json({ message: "No posts found" });
+    }
+    return res.json({ message: "Posts fetched successfully", posts });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Failed to fetch posts", error: error.message });
+  }
+};
+
+const getPostById = async (req, res) => {
+  try {
+    const post = await prisma.post.findFirst({
+      where: {
+        postId: req.params.id,
+      },
+      include: {
+        category: {
+          select: {
+            categoryName: true,
+          },
+        },
+        user: {
+          select: {
+            userName: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+            comment: true,
+            share: true,
+          },
+        },
+      },
+    });
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    return res.json({ message: "Post retrieved successfully", post });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Failed to fetch post", error: error.message });
+  }
+};
+
+module.exports = {
+  createPost,
+  addTagToPost,
+  getPosts,
+  getPostById,
+};
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,3 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const {
+    createPost,
+    addTagToPost,
+    getPosts,
+    getPostById
+  } = require("../controllers/postsControllers");
+  const { checkRole } = require("../middlewares/Autorization");
+  const { authenticateToken } = require("../middlewares/auth");
+  const { validateData } = require("../utils/validation");
+
+router.post("/createPost",
+    authenticateToken,
+    validateData,
+    createPost
+  );
+  router.post("/:postId/addTag",
+    authenticateToken,
+    validateData,
+    addTagToPost
+  );
+
+router.get("/",
+    authenticateToken,
+    validateData,
+    getPosts
+  );
+router.get("/:id",
+    authenticateToken,
+    validateData,
+    getPostById
+  );
 module.exports = router;
