0d6df1cde65fdc22e46571161ef1def173324df5
Bouih32 <enamto.bouih@gmail.com>
2025-03-05 14:22:24 +0000

    fix: update cookie settings to extend expiration to 7 days for user login token

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -97,9 +97,8 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none", // Crucial for cross-site
-      maxAge: expirationDate,
-      domain: ".joinspot-backend.onrender.com",
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // maxAge in milliseconds
       path: "/",
     });
     console.log(res.getHeaders());
