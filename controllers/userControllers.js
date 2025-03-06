50fc821a9e7fc3a25c572089b736e3cf276c9187
Bouih32 <enamto.bouih@gmail.com>
2025-03-06 17:49:54 +0000

    fix: add domain attribute to cookie settings for improved security

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -96,7 +96,8 @@ const loginUser = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // maxAge in milliseconds
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      domain: ".joinspots.com",
       path: "/",
     });
 
